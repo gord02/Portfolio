046a0e2f4be35bba3e59af889581c09dea6d866c
gord02 <53834349+gord02@users.noreply.github.com>
2020-08-20 14:27:16 -0400

    updated resume

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
 
 @app.route("/resume")
 def send_pdf():
-    return send_file('static/pdf/Resume.pdf', attachment_filename='resume.pdf')
+    return send_file('static/pdf/NeWResume.pdf', attachment_filename='resume.pdf')
 
 @app.route("/", methods=["POST"])
 def contacting():
